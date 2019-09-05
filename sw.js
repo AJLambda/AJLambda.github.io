2219682a150a7653c33f1ca44dce14963aa9339e
Austin James <apj2277@gmail.com>
2019-09-05 00:02:52 -0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-626ab27bda53cd7f61ab.js"
+    "url": "webpack-runtime-e1fb2da1d50936ecf508.js"
   },
   {
     "url": "app-b0756724ad7fccc981d5.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "78d838198cfdf27181c0d240a0aee2aa"
+    "revision": "fcec9e473966fdcc7d38b662ae335824"
   },
   {
     "url": "manifest.webmanifest",
