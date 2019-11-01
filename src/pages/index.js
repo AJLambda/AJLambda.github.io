470682f12b0850543726c7d6f82f0c51433d1632
Lambda_School_Loaner_191 <lambda_school_loaner_191@lambdasr191smbp.home>
2019-11-01 16:23:47 -0700

    working on mobile project cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -85,12 +85,13 @@ const IndexPage = () => (
             <div className="projects-container">
               <div className="grid">
                 <Fade>
-                  <a
-                    href="https://review-it-yourself.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <div className="white-box">
+                  <div className="white-box">
+                    <a
+                      href="https://review-it-yourself.com/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="project-anchor"
+                    >
                       <figure className="effect-zoe">
                         <img src={riy} alt="img25" />
 
@@ -127,155 +128,256 @@ const IndexPage = () => (
                           </p>
                         </figcaption>
                       </figure>
-                    </div>
-                  </a>
+                    </a>
+                  </div>
                 </Fade>
               </div>
-              <div className="grid">
-                <Fade>
+
+              {/* Mobile Project */}
+              <div className="mobile-project-card">
+                <div className="mobile-project-img" id="riy"></div>
+                <div className="mobile-project-info">
+                  <h3>Review it Yourself</h3>
+                  <p>
+                    DIY web and iOS app where users can find, create, review and
+                    rate projects.
+                  </p>
                   <a
-                    href="https://safespace-landing.netlify.com/"
+                    href="https://github.com/labs13-how-to"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <div className="white-box">
-                      <figure className="effect-zoe">
-                        <img src={safespace} alt="img25" />
-
-                        <figcaption>
-                          <h2>
-                            Safe <span>Space</span>
-                          </h2>
-                          <div className="icon-links">
-                            <Tooltip TransitionComponent={UiZoom} title="URL">
-                              <a
-                                href="https://safespace-landing.netlify.com/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                              >
-                                <div className="fa fa-link fa-3x" />
-                              </a>
-                            </Tooltip>
-                            <Tooltip TransitionComponent={UiZoom} title="Code">
-                              <a
-                                href="https://github.com/AJLambda/Safespace-landing"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                              >
-                                <FontAwesomeIcon
-                                  icon={faGithub}
-                                  className="github-icon"
-                                />
-                              </a>
-                            </Tooltip>
-                          </div>
-                          <p className="description">
-                            Self-care web app designed to send positive text
-                            reminders throughout the day.
-                          </p>
-                        </figcaption>
-                      </figure>
-                    </div>
+                    <FontAwesomeIcon
+                      icon={faGithub}
+                      className="github-icon-mobile"
+                    />
                   </a>
-                </Fade>
-              </div>
-              <div className="grid">
-                <Fade>
                   <a
-                    href="https://mytopnine.netlify.com"
+                    href="https://review-it-yourself.com/"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <div className="white-box">
-                      <figure className="effect-zoe">
-                        <img src={topnine} alt="img25" />
-
-                        <figcaption>
-                          <h2>
-                            My Top <span>Nine</span>
-                          </h2>
-                          <div className="icon-links">
-                            <Tooltip TransitionComponent={UiZoom} title="URL">
-                              <a
-                                href="https://mytopnine.netlify.com"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                              >
-                                <div className="fa fa-link fa-3x" />
-                              </a>
-                            </Tooltip>
-                            <Tooltip TransitionComponent={UiZoom} title="Code">
-                              <a
-                                href="https://github.com/AJLambda/MyTopNine"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                              >
-                                <FontAwesomeIcon
-                                  icon={faGithub}
-                                  className="github-icon"
-                                />
-                              </a>
-                            </Tooltip>
-                          </div>
-                          <p className="description">
-                            Social web app where users can select their Top-Nine
-                            in a variety of categories.
-                            <br />
-                            (Landing Page)
-                          </p>
-                        </figcaption>
-                      </figure>
-                    </div>
+                    <div className="fa fa-link fa-3x" />
                   </a>
-                </Fade>
+                </div>
               </div>
+
               <div className="grid">
                 <Fade>
+                  <div className="white-box">
+                    <figure className="effect-zoe">
+                      <img src={safespace} alt="img25" />
+
+                      <figcaption>
+                        <h2>
+                          Safe <span>Space</span>
+                        </h2>
+                        <div className="icon-links">
+                          <Tooltip TransitionComponent={UiZoom} title="URL">
+                            <a
+                              href="https://safespace-landing.netlify.com/"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <div className="fa fa-link fa-3x" />
+                            </a>
+                          </Tooltip>
+                          <Tooltip TransitionComponent={UiZoom} title="Code">
+                            <a
+                              href="https://github.com/AJLambda/Safespace-landing"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <FontAwesomeIcon
+                                icon={faGithub}
+                                className="github-icon"
+                              />
+                            </a>
+                          </Tooltip>
+                        </div>
+                        <p className="description">
+                          Self-care web app designed to send positive text
+                          reminders throughout the day.
+                        </p>
+                      </figcaption>
+                    </figure>
+                  </div>
+                </Fade>
+              </div>
+
+              {/* Mobile Project */}
+              <div className="mobile-project-card">
+                <div className="mobile-project-img" id="safespace"></div>
+                <div className="mobile-project-info">
+                  <h3>Review it Yourself</h3>
+                  <p>
+                    DIY web and iOS app where users can find, create, review and
+                    rate projects.
+                  </p>
                   <a
-                    href="https://lambdamudd.netlify.com/"
+                    href="https://github.com/labs13-how-to"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <div className="white-box">
-                      <figure className="effect-zoe">
-                        <img src={lambdamud} alt="img25" />
-
-                        <figcaption>
-                          <h2>
-                            Lambda <span>Mudd</span>
-                          </h2>
-                          <div className="icon-links">
-                            <Tooltip TransitionComponent={UiZoom} title="URL">
-                              <a
-                                href="https://lambdamudd.netlify.com/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                              >
-                                <div className="fa fa-link fa-3x" />
-                              </a>
-                            </Tooltip>
-                            <Tooltip TransitionComponent={UiZoom} title="Code">
-                              <a
-                                href="https://github.com/CS20-BuildWeek-1"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                              >
-                                <FontAwesomeIcon
-                                  icon={faGithub}
-                                  className="github-icon"
-                                />
-                              </a>
-                            </Tooltip>
-                          </div>
-                          <p className="description">
-                            Multi-User-Dungeon web app game with a custom made
-                            map, room chat, and retro Fx.
-                          </p>
-                        </figcaption>
-                      </figure>
-                    </div>
+                    <FontAwesomeIcon
+                      icon={faGithub}
+                      className="github-icon-mobile"
+                    />
                   </a>
+                  <a
+                    href="https://review-it-yourself.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="fa fa-link fa-3x" />
+                  </a>
+                </div>
+              </div>
+
+              <div className="grid">
+                <Fade>
+                  <div className="white-box">
+                    <figure className="effect-zoe">
+                      <img src={topnine} alt="img25" />
+
+                      <figcaption>
+                        <h2>
+                          My Top <span>Nine</span>
+                        </h2>
+                        <div className="icon-links">
+                          <Tooltip TransitionComponent={UiZoom} title="URL">
+                            <a
+                              href="https://mytopnine.netlify.com"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <div className="fa fa-link fa-3x" />
+                            </a>
+                          </Tooltip>
+                          <Tooltip TransitionComponent={UiZoom} title="Code">
+                            <a
+                              href="https://github.com/AJLambda/MyTopNine"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <FontAwesomeIcon
+                                icon={faGithub}
+                                className="github-icon"
+                              />
+                            </a>
+                          </Tooltip>
+                        </div>
+                        <p className="description">
+                          Social web app where users can select their Top-Nine
+                          in a variety of categories.
+                          <br />
+                          (Landing Page)
+                        </p>
+                      </figcaption>
+                    </figure>
+                  </div>
                 </Fade>
+              </div>
+
+              {/* Mobile Project */}
+              <div className="mobile-project-card">
+                <div className="mobile-project-img" id="mytopnine"></div>
+                <div className="mobile-project-info">
+                  <h3>Review it Yourself</h3>
+                  <p>
+                    DIY web and iOS app where users can find, create, review and
+                    rate projects.
+                  </p>
+                  <a
+                    href="https://github.com/labs13-how-to"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FontAwesomeIcon
+                      icon={faGithub}
+                      className="github-icon-mobile"
+                    />
+                  </a>
+                  <a
+                    href="https://review-it-yourself.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="fa fa-link fa-3x" />
+                  </a>
+                </div>
+              </div>
+
+              <div className="grid">
+                <Fade>
+                  <div className="white-box">
+                    <figure className="effect-zoe">
+                      <img src={lambdamud} alt="img25" />
+
+                      <figcaption>
+                        <h2>
+                          Lambda <span>Mudd</span>
+                        </h2>
+                        <div className="icon-links">
+                          <Tooltip TransitionComponent={UiZoom} title="URL">
+                            <a
+                              href="https://lambdamudd.netlify.com/"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <div className="fa fa-link fa-3x" />
+                            </a>
+                          </Tooltip>
+                          <Tooltip TransitionComponent={UiZoom} title="Code">
+                            <a
+                              href="https://github.com/CS20-BuildWeek-1"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <FontAwesomeIcon
+                                icon={faGithub}
+                                className="github-icon"
+                              />
+                            </a>
+                          </Tooltip>
+                        </div>
+                        <p className="description">
+                          Multi-User-Dungeon web app game with a custom made
+                          map, room chat, and retro Fx.
+                        </p>
+                      </figcaption>
+                    </figure>
+                  </div>
+                </Fade>
+              </div>
+
+              {/* Mobile Project */}
+              <div className="mobile-project-card">
+                <div className="mobile-project-img" id="lambdamud"></div>
+                <div className="mobile-project-info">
+                  <h3>Review it Yourself</h3>
+                  <p>
+                    DIY web and iOS app where users can find, create, review and
+                    rate projects.
+                  </p>
+                  <a
+                    href="https://github.com/labs13-how-to"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FontAwesomeIcon
+                      icon={faGithub}
+                      className="github-icon-mobile"
+                    />
+                  </a>
+                  <a
+                    href="https://review-it-yourself.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="fa fa-link fa-3x" />
+                  </a>
+                </div>
               </div>
             </div>
           </div>
